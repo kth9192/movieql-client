949ffe214890ee2a26f578b49307538c69be1a22
kth919 <kth9193@gmail.com>
2019-12-18 15:59:04 +0900

    cache

--- src/Detail.js
+++ src/Detail.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { withRouter } from "react-router-dom";
 import { Query } from "react-apollo";
+import { MOVIE_DETAILS } from "./queries";
 
 const Detail = ({
   match: {
@@ -10,7 +11,24 @@ const Detail = ({
   console.log("====================================");
   console.log(movieId);
   console.log("====================================");
-  return <div>Detail</div>;
+  return (
+    <Query query={MOVIE_DETAILS} variables={{ movieId: parseInt(movieId) }}>
+      {({ loading, error, data }) => {
+        if (loading) return "loading";
+        if (error) return "error";
+        return (
+          <Fragment>
+            {data.movie.medium_cover_image}
+            {data.movie.title}
+            {data.movie.rating}
+            {data.movie.description_intro}
+            {data.movie.language}
+            {data.movie.genres}
+          </Fragment>
+        );
+      }}
+    </Query>
+  );
 };
 
 export default withRouter(Detail);
